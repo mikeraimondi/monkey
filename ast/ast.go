c64cee638cc7c2457c94e0c03d2cb600e046222b
Michael Raimondi <mike@mikeraimondi.com>
2018-08-21 22:39:16 -0400

    infix parsing

--- ast/ast.go
+++ ast/ast.go
@@ -38,10 +38,10 @@ func (p *Program) TokenLiteral() string {
 }
 
 func (p *Program) String() string {
-	out := strings.Builder{}
+	out := StringBuilder{}
 
 	for _, s := range p.Statements {
-		out.WriteString(s.String())
+		out.MustWrite(s.String())
 	}
 
 	return out.String()
@@ -54,21 +54,23 @@ type LetStatement struct {
 	Value Expression
 }
 
-func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) statementNode() {}
+
+// TokenLiteral is used for debugging
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
 func (ls *LetStatement) String() string {
-	out := strings.Builder{}
+	out := StringBuilder{}
 
-	out.WriteString(ls.TokenLiteral() + " ")
-	out.WriteString(ls.Name.String())
-	out.WriteString(" = ")
+	out.MustWrite(ls.TokenLiteral() + " ")
+	out.MustWrite(ls.Name.String())
+	out.MustWrite(" = ")
 
 	if ls.Value != nil {
-		out.WriteString(ls.Value.String())
+		out.MustWrite(ls.Value.String())
 	}
 
-	out.WriteString(";")
+	out.MustWrite(";")
 
 	return out.String()
 }
@@ -79,7 +81,9 @@ type Identifier struct {
 	Value string
 }
 
-func (i *Identifier) expressionNode()      {}
+func (i *Identifier) expressionNode() {}
+
+// TokenLiteral is used for debugging
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 func (i *Identifier) String() string       { return i.Value }
 
@@ -89,30 +93,34 @@ type ReturnStatement struct {
 	ReturnValue Expression
 }
 
-func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) statementNode() {}
+
+// TokenLiteral is used for debugging
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
 
 func (rs *ReturnStatement) String() string {
-	out := strings.Builder{}
+	out := StringBuilder{}
 
-	out.WriteString(rs.TokenLiteral() + " ")
+	out.MustWrite(rs.TokenLiteral() + " ")
 
 	if rs.ReturnValue != nil {
-		out.WriteString(rs.ReturnValue.String())
+		out.MustWrite(rs.ReturnValue.String())
 	}
 
-	out.WriteString(";")
+	out.MustWrite(";")
 
 	return out.String()
 }
 
-// ExpressonStatement is a statement that produces a value
+// ExpressionStatement is a statement that produces a value
 type ExpressionStatement struct {
 	Token      token.Token // the first token of the expression
 	Expression Expression
 }
 
-func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) statementNode() {}
+
+// TokenLiteral is used for debugging
 func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
 
 func (es *ExpressionStatement) String() string {
@@ -122,30 +130,72 @@ func (es *ExpressionStatement) String() string {
 	return ""
 }
 
+// PrefixExpression is an expression with an operator that precedes an operand
 type PrefixExpression struct {
 	Token    token.Token // the prefix token
 	Operator string
 	Right    Expression
 }
 
-func (pe *PrefixExpression) expressionNode()      {}
+func (pe *PrefixExpression) expressionNode() {}
+
+// TokenLiteral is used for debugging
 func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
 func (pe *PrefixExpression) String() string {
-	out := strings.Builder{}
+	out := StringBuilder{}
 
-	out.WriteString("(")
-	out.WriteString(pe.Operator)
-	out.WriteString(pe.Right.String())
-	out.WriteString(")")
+	out.MustWrite("(")
+	out.MustWrite(pe.Operator)
+	out.MustWrite(pe.Right.String())
+	out.MustWrite(")")
 
 	return out.String()
 }
 
+// InfixExpression is an expression with an operator between two operands
+type InfixExpression struct {
+	Token    token.Token // the operator token
+	Left     Expression
+	Operator string
+	Right    Expression
+}
+
+func (ie *InfixExpression) expressionNode() {}
+
+// TokenLiteral is used for debugging
+func (ie *InfixExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *InfixExpression) String() string {
+	out := StringBuilder{}
+
+	out.MustWrite("(")
+	out.MustWrite(ie.Left.String())
+	out.MustWrite(" " + ie.Operator + " ")
+	out.MustWrite(ie.Right.String())
+	out.MustWrite(")")
+
+	return out.String()
+}
+
+// IntegerLiteral is an integer
 type IntegerLiteral struct {
 	Token token.Token
 	Value int64
 }
 
-func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) expressionNode() {}
+
+// TokenLiteral is used for debugging
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string       { return il.Token.Literal }
+
+// StringBuilder is a convenience wrapper around strings.Builder
+type StringBuilder struct {
+	strings.Builder
+}
+
+// MustWrite panics if WriteString is unsuccesful. This should "never happen".
+func (builder *StringBuilder) MustWrite(in string) {
+	if _, err := builder.WriteString(in); err != nil {
+		panic("error writing string to builder: " + err.Error())
+	}
+}
