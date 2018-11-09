12c0700ae5351520e76685ba3294e4b00a8c2c23
Michael Raimondi <mike@mikeraimondi.com>
2018-11-08 21:47:43 -0500

    bang and minus operators

--- code/code.go
+++ code/code.go
@@ -18,6 +18,8 @@ const (
 	OpEqual
 	OpNotEqual
 	OpGreaterThan
+	OpMinus
+	OpBang
 )
 
 var definitions = map[Opcode]*Definition{
@@ -32,6 +34,8 @@ var definitions = map[Opcode]*Definition{
 	OpEqual:       {"OpEqual", []int{}},
 	OpNotEqual:    {"OpNotEqual", []int{}},
 	OpGreaterThan: {"OpGreaterThan", []int{}},
+	OpMinus:       {"OpMinus", []int{}},
+	OpBang:        {"OpBang", []int{}},
 }
 
 type Definition struct {
